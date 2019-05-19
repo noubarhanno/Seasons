11d6a33bc52c955674036275a30c6dec63679b56
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-05-19 17:06:23 +0400

    refactor the code to use hooks with extracting the business login

--- src/index.js
+++ src/index.js
@@ -1,35 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import useLocation from "./useLocation";
 
-class App extends React.Component{
-    state = {lat:null, errorMessage:''}
+const App = () => {
+  const [lat, errorMessage] = useLocation();
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept the location service" />;
+  }
 
-    componentDidMount(){
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({lat: position.coords.latitude}),
-            err => this.setState({errorMessage: err.message})
-        )
-    }
+  return <div className="border red">{content}</div>;
+};
 
-    seasonContent(){
-        if (this.state.lat && !this.state.errorMessage){
-            return <SeasonDisplay lat={this.state.lat} />;
-        }
-        if (!this.state.lat && this.state.errorMessage){
-            return <div><h1>Error: {this.state.errorMessage}</h1></div>;
-        }
-        return <Spinner message='Please accept the location service' />
-    }
-
-    render(){
-        return(
-        <div className='container'>
-            {this.seasonContent()}
-        </div>
-        );
-    }
-}
-
-ReactDOM.render(<App /> , document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
